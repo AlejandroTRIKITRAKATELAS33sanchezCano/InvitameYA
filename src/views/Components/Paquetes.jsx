629ce5526d8003f4b2d37d7f74281795a23ff1f3
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2023-12-24 23:56:43 -0600

    Se Mejoran Estilos, Se Añaden Funciones Responsive De Los Paquetes

--- src/views/Components/Paquetes.jsx
+++ src/views/Components/Paquetes.jsx
@@ -3,111 +3,37 @@ import Paquete from "./microComponets/Paquete";
 const Paquetes = () => {
     return (
         <section className='PaquetesGeneral'>
-            <h2>Selecciona La Mejor Invitación Para ti</h2>
+            <h2>¡Descubre los Paquetes Perfectos para Ti!</h2>
             <div className="Paquetes">
-                <Paquete/>
+                <Paquete />
             </div>
             <div className="Quetienen">
                 <table>
-                    <tr>
-                        <th>Invitación</th>
-                        <th>JPG o PNG</th>
-                        <th>PDF</th>
-                        <th>Video</th>
-                        <th>Web</th>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-
-                    </tr>
-                    <tr>
-
-                    </tr>
-                    <tr>
-
-                    </tr>
-                    <tr>
-
-                    </tr>
-                    <tr>
-
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Invitación</th>
+                            <th>JPG o PNG</th>
+                            <th>PDF</th>
+                            <th>Video</th>
+                            <th>Web</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Paquete 1</td>
+                            <td><span class="checkmark"></span></td>
+                            <td></td>
+                            <td><span class="checkmark"></span></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Paquete 2</td>
+                            <td></td>
+                            <td><span class="checkmark"></span></td>
+                            <td></td>
+                            <td><span class="checkmark"></span></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </section>
--- src/views/Components/microComponets/Paquete.jsx
+++ src/views/Components/microComponets/Paquete.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
+import { paquetes } from "../../../Constants";
 
 const Paquete = () => {
     return (
-        <div className="PaqueteBasico">
-            <h3>Basico</h3>
-            <b>Lorem ipsum dolor sit amet.</b>
-            <div className="precioAnterior">
-                <s>MX $1000</s>
-            </div>
-            <h2>$MX <span>599</span></h2>
-            <Link to="/" className="MuestrasLink">
-                Muestras
-            </Link>
-        </div>
+        <>
+            {
+                paquetes.map((paquete) => (
+                    <>
+                        <div className="PaqueteBasico">
+                            <h3>{paquete.nombre}</h3>
+                            <b>{paquete.descripcion}</b>
+                            <div className="precioAnterior">
+                                <s>MX ${paquete.precioAnterior}</s>
+                            </div>
+                            <h2>MX <span>${paquete.precio}</span></h2>
+                            <Link to={`/${paquete.linkMuestras}`} className="MuestrasLink">
+                                Muestras
+                            </Link>
+                        </div>
+                    </>
+                ))
+            }
+        </>
     );
 }
 
